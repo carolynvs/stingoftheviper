5a86e24b0e3c2c3a87b8f72d01ecd98ac9e28ad9
Carolyn Van Slyck <me@carolynvanslyck.com>
2020-08-09 10:02:22 -0500

    Clarify comment

--- main.go
+++ main.go
@@ -104,13 +104,13 @@ func initializeConfig(cmd *cobra.Command) error {
 func bindFlags(cmd *cobra.Command, v *viper.Viper) {
 	cmd.Flags().VisitAll(func(f *pflag.Flag) {
 		// Environment variables can't have dashes in them, so bind them to their equivalent
-		// keys with underscores, e.g. --debug-plugins binds to PORTER_DEBUG_PLUGINS
+		// keys with underscores, e.g. --favorite-color to STING_FAVORITE_COLOR
 		if strings.Contains(f.Name, "-") {
 			envVarSuffix := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
 			v.BindEnv(f.Name, fmt.Sprintf("%s_%s", envPrefix, envVarSuffix))
 		}
 
-		// Apply the configuration file value to the flag when the flag is not set
+		// Apply the viper config value to the flag when the flag is not set and viper has a value
 		if !f.Changed && v.IsSet(f.Name) {
 			val := v.Get(f.Name)
 			cmd.Flags().Set(f.Name, fmt.Sprintf("%v", val))
